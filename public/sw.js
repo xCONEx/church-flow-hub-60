4c838584b9472a0a9bdd4bbb0486dce1aa9ef3fa
euyuriadriel <60609385+xCONEx@users.noreply.github.com>
2025-06-17 10:21:48 -0300

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,30 @@
+
+const CACHE_NAME = 'church-manager-v1';
+const urlsToCache = [
+  '/',
+  '/static/js/bundle.js',
+  '/static/css/main.css',
+  '/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});

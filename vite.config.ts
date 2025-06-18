1e77cd1e525b4f7cdd1285636d9d039fded98cbf
euyuriadriel <60609385+xCONEx@users.noreply.github.com>
2025-06-18 11:06:15 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/", // <- ESSENCIAL para Vercel encontrar os assets
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === 'development' &&
+    componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
